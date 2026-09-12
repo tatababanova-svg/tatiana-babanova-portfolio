1a20c358517afb064d0637ffd73f67aacb4452ba
tatababanova-svg <tatababanova@gmail.com>
2026-09-12 10:43:41 +0300

    Use the same portrait asset treatment on GitHub Pages

--- components/github-pages-compat.tsx
+++ components/github-pages-compat.tsx
@@ -13,6 +13,15 @@ export function GitHubPagesCompat() {
       .forEach((link) => {
         link.href = `${basePath}/tatiana-babanova-project-manager.pdf`;
       });
+
+    document
+      .querySelectorAll<HTMLImageElement>(".approved-portrait-frame img")
+      .forEach((image) => {
+        image.removeAttribute("srcset");
+        image.srcset = "";
+        image.src = `${basePath}/tatiana-babanova.jpg`;
+        image.style.opacity = "1";
+      });
   }, []);
 
   return null;
